5e7c6b66842e61356818f87b5101a9cc3cb69d93
venusgalstar <venusgalstar@gmail.com>
2022-08-26 18:29:25 +0200

    added golden nft

--- smart-contracts/contracts/PenguManager.sol
+++ smart-contracts/contracts/PenguManager.sol
@@ -18,16 +18,21 @@ contract PenguManaer is ExpiryHelper {
     address owner;
     address penguPalTokenAddress;
     address penguNFTTokenAddress;
+    address penguNFTGoldTokenAddress;
 
     uint256 dailyReward = 100 * 10 ** 8 / (3600 * 24); //pengupal decimal is 8
     uint256 maxNFTPerUser = 100;
     mapping(address => RewardNFTInfo[]) rewardUserInfo;
     mapping(int64 => bool) stakedNFTInfo;
+    
+    mapping(address => RewardNFTInfo[]) rewardUserGoldInfo;
+    mapping(int64 => bool) stakedNFTGoldInfo;
 
-    constructor(address _penguPalTokenAddress, address _penguNFTTokenAddress) {
+    constructor(address _penguPalTokenAddress, address _penguNFTTokenAddress, address _penguNFTGoldTokenAddress) {
         owner = msg.sender;
         penguPalTokenAddress = _penguPalTokenAddress;
         penguNFTTokenAddress = _penguNFTTokenAddress;
+        penguNFTGoldTokenAddress = _penguNFTGoldTokenAddress;
     }
 
     //only owner
@@ -59,6 +64,16 @@ contract PenguManaer is ExpiryHelper {
     function getPenguNFTTokenAddress() external returns(address){
         return penguNFTTokenAddress;
     }
+
+    //only owner
+    function setPenguNFTGoldTokenAddress(address _penguNFTGoldTokenAddress) external {
+        require(owner == msg.sender, "Only Owner Can Call This Function!");
+        penguNFTGoldTokenAddress = _penguNFTGoldTokenAddress;
+    }
+
+    function getPenguNFTGoldTokenAddress() external returns(address){
+        return penguNFTGoldTokenAddress;
+    }
     
     function stakeNFT(int64 nftID) external{
         
@@ -84,7 +99,7 @@ contract PenguManaer is ExpiryHelper {
         stakedNFTInfo[nftID] = true;
     }
 
-    function unStakeNFT(int64 nftID) external{
+    function unstakeNFT(int64 nftID) external{
         require(stakedNFTInfo[nftID] == true, "Not Staked!");
 
         uint256 idx;
@@ -118,6 +133,64 @@ contract PenguManaer is ExpiryHelper {
         }
     }
 
+    function stakeGoldNFT(int64 nftID) external{
+        
+        require(stakedNFTGoldInfo[nftID] == false, "Already Staked!");
+
+        //check msg.sender is the owner of this nft and transfer nft to this contract
+        int response1 = HederaTokenService.associateToken(address(this), penguNFTGoldTokenAddress);
+
+        require(response1 == SUCCESS, "Failed to Associate NFT this contract");
+
+        int response2 = HederaTokenService.transferNFT(penguNFTGoldTokenAddress, msg.sender, address(this), nftID);
+
+        require(response2 == SUCCESS, "Failed to Transfer NFT to this contract");
+
+        RewardNFTInfo memory newStakedNFT;
+        newStakedNFT.id = nftID;
+        newStakedNFT.startTime = block.timestamp;
+        newStakedNFT.lastClaimTime = block.timestamp;
+        newStakedNFT.owner = msg.sender;
+
+        rewardUserGoldInfo[msg.sender].push(newStakedNFT);
+
+        stakedNFTGoldInfo[nftID] = true;
+    }
+
+    function unstakeGoldNFT(int64 nftID) external{
+        require(stakedNFTGoldInfo[nftID] == true, "Not Staked!");
+
+        uint256 idx;
+        int response1;
+        int response2;
+
+        for( idx = 0; idx < rewardUserGoldInfo[msg.sender].length; idx++ )
+        {
+            if( rewardUserGoldInfo[msg.sender][idx].id != nftID || rewardUserGoldInfo[msg.sender][idx].owner != msg.sender )
+                continue;
+            //give reward to user, 
+            uint256 rewardAmount = (block.timestamp - rewardUserGoldInfo[msg.sender][idx].lastClaimTime) * dailyReward;
+
+            response1 = HederaTokenService.associateToken(address(this), penguPalTokenAddress);
+            require(response1 == SUCCESS, "Failed to Associate Pengupal this contract");
+    
+            response2 = HederaTokenService.transferToken(penguPalTokenAddress, address(this), msg.sender, rewardAmount);    
+            require(response2 == SUCCESS, "Failed to Transfer Pengupal to msg.sender");
+
+            //transfer nft to user
+            response1 = HederaTokenService.associateToken(address(this), penguPalGoldTokenAddress);
+            require(response1 == SUCCESS, "Failed to Associate NFT this contract");
+    
+            response2 = HederaTokenService.transferNFT(penguPalGoldTokenAddress, address(this), msg.sender, rewardUserGoldInfo[msg.sender][idx].id);    
+            require(response2 == SUCCESS, "Failed to Transfer NFT to this contract");
+
+            rewardUserGoldInfo[msg.sender][idx] = rewardUserGoldInfo[msg.sender][rewardUserGoldInfo[msg.sender].length -1];
+            rewardUserGoldInfo[msg.sender].pop();
+
+            stakedNFTGoldInfo[nftID] = false;
+        }
+    }
+
     function claim() external{
 
         uint256 idx;
@@ -129,6 +202,14 @@ contract PenguManaer is ExpiryHelper {
             //check goldenNFTCount
             rewardSum += (block.timestamp - rewardUserInfo[msg.sender][idx].lastClaimTime) * dailyReward;
             rewardUserInfo[msg.sender][idx].lastClaimTime = block.timestamp;            
+        }
+
+        for(idx = 0; idx < rewardUserGoldInfo[msg.sender].length; idx++)
+        {
+            //check goldenNFTCount
+            rewardSum += (block.timestamp - rewardUserGoldInfo[msg.sender][idx].lastClaimTime) * dailyReward;
+            rewardUserGoldInfo[msg.sender][idx].lastClaimTime = block.timestamp;   
+            goldenNFTCount++;         
         }
 
         if( goldenNFTCount > 0 )
